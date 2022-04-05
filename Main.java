ca8b26b5778f1a2cf1a8d5aee1aab90ad32b584c
Mr-polo11polo11 <101180608+WASHI2000@users.noreply.github.com>
2022-04-05 10:02:50 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+  String q = 'q';
+  public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+  String znak = scan.next().charAt(0);
+    for (;;)
+      { 
+        if (znak && q)    break;
+    {
+  System.out.print(znak);
+  }
+  }
+  }
+}
